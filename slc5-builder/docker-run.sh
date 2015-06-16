c36dfed8a86574d90e7f1b631bc11912a52b5765
Dario Berzano <dario.berzano@cern.ch>
2015-06-16 16:51:37 +0200

    SLC5: moved build script to ali-bot

--- slc5-builder/docker-run.sh
+++ slc5-builder/docker-run.sh
@@ -11,22 +11,22 @@ PackagesDirHost="${HOME}/alisw/packages"
 PackagesDirGuest='/opt/aliroot/www/tarballs'
 CcacheDirHost="${HOME}/alisw/ccache/slc5-builder"
 CcacheDirGuest='/ccache'
-BuilderHost="${PWD}/wrap-gar-build.sh"
-BuilderGuest='/wrap-gar-build.sh'
-RecipeSvnCredsHost="${PWD}/recipe-svn-creds.txt"
-RecipeSvnCredsGuest='/recipe-svn-creds.txt'
+BuilderHost="$( cd ${PWD}/../.. ; pwd )/ali-bot/build-ib.sh"
+BuilderGuest='/build-ib.sh'
+RecipeSvnCreds="$( cat "${PWD}/recipe-svn-creds.txt" 2> /dev/null )"
+RecipeSvnUser=${RecipeSvnCreds%%:*}
+RecipeSvnPassword=${RecipeSvnCreds#*:}
 
 mkdir -p "$PackagesDirHost" "$CcacheDirHost"
 
 exec time docker run -it --rm \
   -v "${BuilderHost}:${BuilderGuest}:ro" \
-  -v "${RecipeSvnCredsHost}:${RecipeSvnCredsGuest}:ro" \
   -v "${PackagesDirHost}:${PackagesDirGuest}:rw" \
   -v "${CcacheDirHost}:${CcacheDirGuest}:rw" \
   -e 'PS1=`[ $? == 0 ] || echo $?\|`'$Container' ~ \w \u@\h \$> ' \
   -e "CCACHE_DIR=${CcacheDirGuest}" \
   -e "HOME=/root" \
+  -e "DEFAULT_RECIPE_USER=${RecipeSvnUser}" \
+  -e "DEFAULT_RECIPE_PASS=${RecipeSvnPassword}" \
   "$Container" \
-  "$BuilderGuest" \
-    --software root \
-    --recipe-version v5-06-25-01
+  "$BuilderGuest" "$@"
