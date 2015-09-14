dccd601d679731262f77c818f90222ac892312ca
Giulio Eulisse <giulio.eulisse@cern.ch>
2015-09-14 10:25:41 +0200

    Better support for Monalisa and development mode
    - Add ability to specify MONALISA_HOST and MONALISA_PORT so that
      it can get substituted.
    - Do not clone ali-bot configuration if directory is already there.

--- logstash/run.sh
+++ logstash/run.sh
@@ -2,10 +2,16 @@
 
 REPO=${LOGSTASH_REPO-alisw}
 
-git clone https://github.com/$REPO/ali-bot
-
-if [ ! "X$DEBUG" = X ]; then
-  echo 'output {stdout { codec => rubydebug }}' >> /ali-bot/logstash/logstash.conf
+if [ ! -d /ali-bot ]; then
+  git clone https://github.com/$REPO/ali-bot
 fi
 
-/opt/logstash/bin/logstash -f /ali-bot/logstash/logstash.conf
+cp /ali-bot/logstash/logstash.conf /logstash.conf
+perl -p -i -e "s/MONALISA_HOST/$MONALISA_HOST/" /logstash.conf
+perl -p -i -e "s/MONALISA_PORT/$MONALISA_PORT/" /logstash.conf
+
+if [ ! "X$DEBUG" = X ]; then
+  echo 'output {stdout { codec => rubydebug }}' >> /logstash.conf
+fi
+
+/opt/logstash/bin/logstash -f /logstash.conf
