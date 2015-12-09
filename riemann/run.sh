a23fcb416118cf6355d617e6a883c16d578e3436
Giulio Eulisse <giulio.eulisse@cern.ch>
2015-12-09 16:50:11 +0100

    Move configuration out of docks
    Configuration is really ALICE specific, so we move it to ali-bot/riemann
    and make riemann* images configuration agnostic.

---
+++ riemann/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -ex
+# If the riemann folder does not exist, clone it from ali-bot
+if [ ! -d /config ]; then
+  git clone ${ALI_BOT_BRANCH:+-b $ALI_BOT_BRANCH} https://github.com/${ALI_BOT_REPO:-alisw/ali-bot} /config
+fi
+/riemann-0.2.10/bin/riemann /config/riemann/riemann.config
