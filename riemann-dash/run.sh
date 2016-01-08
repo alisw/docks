ca24bc7af3f3163ac561c894402ab81bf89c93f7
Giulio Eulisse <giulio.eulisse@cern.ch>
2016-01-08 16:13:03 +0100

    Run riemann-dash from sources

--- riemann-dash/run.sh
+++ riemann-dash/run.sh
@@ -4,4 +4,11 @@ if [ ! -d /config ]; then
   git clone ${ALI_BOT_BRANCH:+-b $ALI_BOT_BRANCH} https://github.com/${ALI_BOT_REPO:-alisw/ali-bot} /config
 fi
 
-RIEMANN_DASH_CONFIG=/config/riemann/config.rb riemann-dash
+if [ ! -d /riemann-dash ]; then
+  git clone https://github.com/${REPO:-aphyr/riemann-dash} /riemann-dash
+fi
+
+cd /riemann-dash
+git show
+bundle install
+RIEMANN_DASH_CONFIG=/config/riemann/config.rb bundle exec riemann-dash
