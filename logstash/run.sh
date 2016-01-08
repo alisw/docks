466dcbb48bd5f54de6528edc655b9043e4bbae67
Giulio Eulisse <giulio.eulisse@cern.ch>
2016-01-08 16:10:40 +0100

    Split logstash configuration in several files

--- logstash/run.sh
+++ logstash/run.sh
@@ -1,17 +1,35 @@
 #!/bin/sh -ex
 
-REPO=${LOGSTASH_REPO-alisw}
+REPO=${LOGSTASH_REPO-alisw/ali-bot}
+CONFIG_DIR=${LOGSTASH_CONFIG_DIR-logstash}
 
-if [ ! -d /ali-bot ]; then
-  git clone https://github.com/$REPO/ali-bot
+if [ ! -d /config-ro ]; then
+  git clone https://github.com/$REPO config-ro
 fi
 
-cp /ali-bot/logstash/logstash.conf /logstash.conf
-perl -p -i -e "s/MONALISA_HOST/$MONALISA_HOST/" /logstash.conf
-perl -p -i -e "s/MONALISA_PORT/$MONALISA_PORT/" /logstash.conf
+# Specifing the varius *_HOST variables will enable their related configuration.
+mkdir -p /config
+[ ! X$ELASTICSEARCH_HOST = X ] && cp /config-ro/${CONFIG_DIR}/*-elasticsearch*.conf /config
+[ ! X$MESOS_HOST = X ] && cp /config-ro/${CONFIG_DIR}/*-mesos*.conf /config
+[ ! X$MARATHON_HOST = X ] && cp /config-ro/${CONFIG_DIR}/*-marathon*.conf /config
+[ ! X$MONALISA_HOST = X ] && cp /config-ro/${CONFIG_DIR}/*-monalisa*.conf /config
+[ ! X$RIEMANN_HOST = X ] && cp /config-ro/${CONFIG_DIR}/*-riemann*.conf /config
+[ ! X$REDIS_HOST = X ] && cp /config-ro/${CONFIG_DIR}/*-redis*.conf /config
+cp /config-ro/${CONFIG_DIR}/*-all*.conf /config
+
+# Substitute variables in configuration files.
+perl -p -i -e "s/ELASTICSEARCH_HOST/$ELASTICSEARCH_HOST/g" /config/*.conf
+perl -p -i -e "s/MARATHON_HOST/$MARATHON_HOST/g" /config/*.conf
+perl -p -i -e "s/MESOS_HOST/$MESOS_HOST/g" /config/*.conf
+perl -p -i -e "s/MONALISA_HOST/$MONALISA_HOST/g" /config/*.conf
+perl -p -i -e "s/MONALISA_PORT/$MONALISA_PORT/g" /config/*.conf
+perl -p -i -e "s/RIEMANN_HOST/$RIEMANN_HOST/g" /config/*.conf
+perl -p -i -e "s/REDIS_HOST/$REDIS_HOST/g" /config/*.conf
 
 if [ ! "X$DEBUG" = X ]; then
-  echo 'output {stdout { codec => rubydebug }}' >> /logstash.conf
+  cat << EOF > /config/99-output-debug.conf
+output {stdout { codec => rubydebug }}
+EOF
 fi
 
-/opt/logstash/bin/logstash -f /logstash.conf
+/opt/logstash/bin/logstash -f /config
