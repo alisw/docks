aea639d0a38ef2530df90bd7fc3cd82f6e3aeda9
Giulio Eulisse <giulio.eulisse@cern.ch>
2016-03-10 17:39:22 +0100

    Add java to ARM64 image

--- arm64-builder/build.sh
+++ arm64-builder/build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh -ex
 curl -O http://davidlt.web.cern.ch/davidlt/vault/proot/qemu-aarch64
-curl -O http://davidlt.web.cern.ch/davidlt/vault/proot/centos-7.2.1511-aarch64-rootfs.tar.bz2
-tar xjvf centos-7.2.1511-aarch64-rootfs.tar.bz2
+curl -O http://eulisse.web.cern.ch/eulisse/centos-7.2.1511-aarch64-rootfs.tgz
 curl -O http://davidlt.web.cern.ch/davidlt/vault/proot/proot
+tar xzvf centos-7.2.1511-aarch64-rootfs.tgz
+rm -fr centos-7.2.1511-aarch64-rootfs.tar*
+
+chmod +x /qemu-aarch64
+mkdir -p /cvmfs /build
+
 chmod +x /proot
