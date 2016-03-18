884c6fd4fbb8c782a12c36f7a04d9c5c800c4fb0
Giulio Eulisse <giulio.eulisse@cern.ch>
2016-03-18 18:04:21 +0100

    Do not die if user jenkins is there

--- mesos-slave/run.sh
+++ mesos-slave/run.sh
@@ -4,8 +4,8 @@
 #
 # Notice that in case of a frontend, we expose port 53 as well
 # as a valid resource.
-groupadd -g ${JENKINS_GID:-2000} jenkins
-useradd -u ${JENKINS_UID:-501} -g ${JENKINS_GID:-2000} jenkins
+groupadd -g ${JENKINS_GID:-2000} jenkins || true
+useradd -u ${JENKINS_UID:-501} -g ${JENKINS_GID:-2000} jenkins || true
 mesos-slave --master=${MESOS_MASTER_ZK-zk://localhost:2181/mesos}                \
              --work_dir=${MESOS_MASTER_WORKDIR-/var/lib/mesos}                   \
              ${MESOS_SLAVE_FRONTEND+--resources='ports(*):[31000-32000, 53-53]'} \
