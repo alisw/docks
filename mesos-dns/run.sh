380b754fe2f9b9907e11399ead0bba4d22608fdc
Giulio Eulisse <giulio.eulisse@cern.ch>
2016-04-05 10:50:27 +0200

    mesos-dns: Use zookeeper to detect mesos-masters
    Also adds packer file for mesos-dns.

--- mesos-dns/run.sh
+++ mesos-dns/run.sh
@@ -12,9 +12,11 @@ for x in `echo ${MESOS_DNS_RESOLVERS-8.8.8.8} | tr , \\ `; do
 done
 RESOLVERS=`echo $RESOLVERS | sed -e 's/^, //'`
 
+ZK="\"zk\": \"$MESOS_DNS_ZK\","
+
 cat << EOF > /config.json
 {
-  "masters": [${MASTERS}],
+  ${MESOS_DNS_ZK+$ZK}
   "refreshSeconds": ${MESOS_DNS_REFRESH-60},
   "ttl": ${MESOS_DNS_TTL-60},
   "domain": "${MESOS_DNS_DOMAIN-mesos}",
