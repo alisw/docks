2495592d3490b6f99d40dda9adca6bfddb45c923
Giulio Eulisse <giulio.eulisse@cern.ch>
2016-09-15 15:33:04 +0200

    Fix aurora setup (#121)
    - Add aurora package to mesos slave. This is required so that the slave
      can actually launch the executor.
    - Improve configurability of aurora-scheduler.

--- aurora-scheduler/run.sh
+++ aurora-scheduler/run.sh
@@ -5,11 +5,11 @@ mesos-log initialize --path=/var/lib/aurora/scheduler/db && true
 
 /usr/lib/aurora/bin/aurora-scheduler -mesos_master_address=${AURORA_MESOS_MASTERS} \
   -cluster_name=${AURORA_CLUSTER_NAME:-mesos}                  \
-  -serverset_path=/aurora/scheduler                            \
+  -serverset_path=${AURORA_SERVERSET_PATH:-/aurora/scheduler}  \
   -native_log_quorum_size=${QUORUM_SIZE:-1}                    \
   -backup_dir=/var/lib/aurora-backup                           \
   -http_port=8081                                              \
   -native_log_file_path=/var/lib/aurora/scheduler/db           \
-  -thermos_executor_path=${THERMOS_EXECUTOR_PATH:-/dev/null}   \
+  -thermos_executor_path=${THERMOS_EXECUTOR_PATH:-/usr/bin/thermos_executor}  \
   -zk_endpoints=${ZK_ENDPOINTS:-127.0.0.1:2181}                \
-  -native_log_zk_group_path=/aurora/replicated-log
+  -native_log_zk_group_path=${AURORA_NATIVE_LOG_ZK_GROUP_PATH:-/aurora/replicated-log}
