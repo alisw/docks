85d3c082d5d08978d193877bb8b3da52177fe359
Giulio Eulisse <giulio.eulisse@cern.ch>
2017-02-03 15:52:05 +0100

    Add support for HDFS, airflow to mesos-slave

--- mesos-slave/run.sh
+++ mesos-slave/run.sh
@@ -44,6 +44,18 @@ cat <<EOF > /etc/mesos-slave/modules
 }
 EOF
 
+export HADOOP_PREFIX=/opt/hadoop
+export HADOOP_HOME=$HADOOP_PREFIX
+export HADOOP_COMMON_HOME=$HADOOP_PREFIX
+export HADOOP_CONF_DIR=$HADOOP_PREFIX/etc/hadoop
+export HADOOP_HDFS_HOME=$HADOOP_PREFIX
+export HADOOP_MAPRED_HOME=$HADOOP_PREFIX
+export HADOOP_YARN_HOME=$HADOOP_PREFIX
+export PATH=$PATH:$HADOOP_PREFIX/sbin:$HADOOP_PREFIX/bin
+export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk-1.8.0.121-0.b13.el7_3.x86_64
+
+pip install airflow[hdfs,kerberos]
+
 mesos-slave --master=${MESOS_MASTER_ZK-zk://localhost:2181/mesos}                \
              --work_dir=${MESOS_MASTER_WORKDIR-/var/lib/mesos}                   \
              ${MESOS_SLAVE_FRONTEND+--resources='ports(*):[31000-32000, 53-53]'} \
