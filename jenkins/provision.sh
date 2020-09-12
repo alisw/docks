faf7adbdf0b42395d7d1f4b843fadc3e9ea24fc7
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2020-09-12 15:10:55 +0200

    Add Jenkins docker container

--- jenkins/provision.sh
+++ jenkins/provision.sh
@@ -1,15 +1,14 @@
 rpm -Uvh http://repos.mesosphere.com/el/7/noarch/RPMS/mesosphere-el-repo-7-3.noarch.rpm
-curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 
 yum update -y
 useradd -u 203 -g 1992 jenkins
 
 yum install -y cyrus-sasl java-1.8.0-headless which bash
 yum install -y mesos-1.0.4 
-yum install -y jenkins
 yum install -y fontconfig dejavu-sans-fonts
 
-yum versionlock add jenkins mesos
+mkdir -p /usr/lib/jenkins
+curl -o /usr/lib/jenkins/jenkins.war https://get.jenkins.io/war-stable/2.46.3/jenkins.war
+yum versionlock add mesos
 
 yum clean all -y
