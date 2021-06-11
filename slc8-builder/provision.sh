8a2857cfc4c95b4ae25796f92426f67520d63eeb
Teo Mrnjavac <teo.m@cern.ch>
2021-06-11 14:27:50 +0200

    Add apr-util-devel & cyrus-sasl-devel to slc8 env

--- slc8-builder/provision.sh
+++ slc8-builder/provision.sh
@@ -40,7 +40,8 @@ yum install -y bc e2fsprogs                                        \
                xorg-x11-server-Xorg xorg-x11-xauth xorg-x11-apps   \
                python2 python2-devel rsync libXrandr-devel         \
                libXi-devel libXcursor-devel libXinerama-devel      \
-               gettext-devel rclone s3cmd
+               gettext-devel rclone s3cmd apr-util-devel           \
+               cyrus-sasl-devel 
 
 alternatives --set python /usr/bin/python3
 
