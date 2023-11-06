88330e05c154a2553fded324910c43db9bc368c6
Timo Wilken <timo.wilken@cern.ch>
2023-11-06 15:51:47 +0100

    Add Alma 9 build container (#250)
    This is the `[registry.cern.ch/]alisw/slc9-builder` container.
    
    I've cut down the number of installed packages compared to slc8-builder:
    
    * Build requirements are tracked in alice-o2-full-deps, so reuse that package.
      This way we also have the same setup in the container and for users' local
      builds, easing debugging.
    * Java is not required since we always use the one from `alidist/jdk.sh`, and
      JDK 8 is too old to be used for JAliEn anyway.
    * The extra development tools and X11 libraries were a holdover from the
      alidock days, and are not used any more.
    * I also removed some RPM repo-specific tools like `fpm` and `createrepo`,
      since we use the slc8 container for those at the moment, and in future I'd
      like to move that to a separate `alisw/publisher` container, to save space
      in this one.
    
    I also based the container on the CERN Alma 9 base image, to have the CERN yum
    repos already installed in addition to the standard ones.

---
+++ slc9-builder/provision.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -ex
+
+useradd -rmUu 980 mesosalien
+useradd -rmUu 981 mesosci
+useradd -rmUu 982 mesosdaq
+useradd -rmUu 983 mesosuser
+useradd -rmUu 984 mesostest
+
+wipednf () {
+  rpmdb --rebuilddb
+  dnf clean all
+  rm -rf /var/cache/yum
+}
+
+wipednf
+
+dnf install -y epel-release dnf-plugins-core
+dnf update -y
+dnf groups install -y 'Development Tools'
+# python3-{pip,setuptools} and s3cmd needed for Jenkins builds.
+dnf install -y alice-o2-full-deps python3-pip python3-setuptools s3cmd
+
+wipednf
