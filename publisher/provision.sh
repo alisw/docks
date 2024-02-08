12bb8370f38347914d6e68210993f1fe803ebb2e
Timo Wilken <timo.wilken@cern.ch>
2024-02-08 17:30:21 +0100

    Install requirements for aliPublishS3 directly
    This saves us from having to do a potentially-fragile "pip install" in every
    publishing run.

--- publisher/provision.sh
+++ publisher/provision.sh
@@ -2,8 +2,8 @@
 
 microdnf install -y epel-release  # for s3cmd
 microdnf update -y
-microdnf install -y shadow-utils git curl s3cmd rpm-build createrepo \
-         python3 python3-pip python3-setuptools rubygems
+microdnf install -y shadow-utils git curl s3cmd rpm-build createrepo rubygems \
+         python3 python3-pip python3-setuptools python3-requests python3-pyyaml python3-boto3
 alternatives --set python /usr/bin/python3
 
 gem install --no-document fpm
