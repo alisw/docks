348f4050a448e10053b38df991e8537781dbca53
Timo Wilken <timo.wilken@cern.ch>
2024-02-08 18:58:19 +0100

    Install which for RPM publisher

--- publisher/provision.sh
+++ publisher/provision.sh
@@ -2,7 +2,7 @@
 
 microdnf install -y epel-release  # for s3cmd
 microdnf update -y
-microdnf install -y shadow-utils git curl s3cmd rpm-build createrepo rubygems \
+microdnf install -y shadow-utils git curl which s3cmd rpm-build createrepo rubygems \
          python3 python3-pip python3-setuptools python3-requests python3-pyyaml python3-boto3
 alternatives --set python /usr/bin/python3
 
