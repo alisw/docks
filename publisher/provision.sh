51177438194b689f868611e18ec382ea86e9bf15
Sergio <sergio.garcia.de.la.cruz@cern.ch>
2024-06-10 10:44:11 +0200

    Update publisher packer image to almalinux 9

--- publisher/provision.sh
+++ publisher/provision.sh
@@ -2,9 +2,8 @@
 
 microdnf install -y epel-release  # for s3cmd
 microdnf update -y
-microdnf install -y shadow-utils git curl which s3cmd rpm-build createrepo rubygems \
+microdnf install -y shadow-utils git which s3cmd rpm-build make gcc createrepo rubygems ruby-devel \
          python3 python3-pip python3-setuptools python3-requests python3-pyyaml python3-boto3
-alternatives --set python /usr/bin/python3
 
 gem install --no-document fpm
 
