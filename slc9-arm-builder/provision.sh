c1fc762cd15aa899e2694f5d994236ac1fb1e476
Sergio <sergio.garcia.de.la.cruz@cern.ch>
2024-08-08 16:09:49 +0200

    slc9-arm: Do not rebuild rpm db on provision
    Removing this because it gives problems when building with Docker's overlayfs
    
    Similar issue: https://github.com/radiasoft/containers/issues/91
    
    strace out:
    ```
    renameat(AT_FDCWD, "/var/lib/rpm", AT_FDCWD, "/var/lib/rpmold.68") = -1 EXDEV (Invalid cross-device link)
    ```

--- slc9-arm-builder/provision.sh
+++ slc9-arm-builder/provision.sh
@@ -6,14 +6,6 @@ useradd -rmUu 982 mesosdaq
 useradd -rmUu 983 mesosuser
 useradd -rmUu 984 mesostest
 
-wipednf () {
-  rpmdb --rebuilddb
-  dnf clean all
-  rm -rf /var/cache/yum
-}
-
-wipednf
-
 dnf install -y epel-release dnf-plugins-core
 
 # Formerly "powertools"; contains some dev tools we need.
@@ -24,5 +16,3 @@ dnf update -y
 dnf groups install -y 'Development Tools'
 # python3-{pip,setuptools} and s3cmd needed for Jenkins builds.
 dnf install -y alice-o2-full-deps python3-pip python3-setuptools s3cmd
-
-wipednf
