60a73068a3362e13a9304eab3fa4d37b87fd047c
Sergio <sergio.garcia.de.la.cruz@cern.ch>
2024-12-03 17:18:40 +0100

    slc9-builder: Add GNU `time`
    Adding it to fix this error in the DPG tester:
    
    ```
    /sw/slc9_x86-64/O2/1816-slc9_x86-64-local1/share/scripts/jobutils.sh: line 156: /usr/bin/time: No such file or directory
    ```
    
    @swenzel: [we use the binary over the shell buiting because] It provides
    more information.
    As a gnu tool it's also platform independent

--- slc9-builder/provision.sh
+++ slc9-builder/provision.sh
@@ -23,6 +23,6 @@ dnf config-manager --set-enabled crb
 dnf update -y
 dnf groups install -y 'Development Tools'
 # python3-{pip,setuptools} and s3cmd needed for Jenkins builds.
-dnf install -y alice-o2-full-deps python3-pip python3-setuptools s3cmd
+dnf install -y alice-o2-full-deps python3-pip python3-setuptools s3cmd time
 
 wipednf
