2fab3096ab0433d2f1446d0ca8920a9afce5fbeb
David Rohr <drohr@jwdt.org>
2025-06-02 22:02:11 +0200

    Bump to CUDA 12.9

--- slc9-gpu-builder/provision.sh
+++ slc9-gpu-builder/provision.sh
@@ -18,7 +18,7 @@ CUV=${CUDA_PKG_VERSION}-${CUDA_PKG_VERSION/-/.}.*
 dnf install -y freeglut-devel lsof                                                            \
                "cuda-cudart-$CUV" 'cuda-compat-12-0-*' "cuda-libraries-$CUV" "cuda-nvtx-$CUV" \
                "cuda-libraries-devel-$CUV" "cuda-nvml-devel-$CUV" "cuda-minimal-build-$CUV"   \
-               "cuda-command-line-tools-$CUV tensorrt-devel tensorrt-libs"                    \
+               "cuda-command-line-tools-$CUV" tensorrt-devel tensorrt-libs                    \
                "cudnn9-cuda-$CUDA_PKG_VERSION"                                                \
                hip-rocclr ocl-icd ocl-icd-devel hipcub rocthrust rocm-dev hipify-clang        \
                hiprand-devel hipblas-devel hipsparse-devel rocblas-devel rocrand-devel        \
@@ -27,11 +27,7 @@ dnf install -y freeglut-devel lsof                                              
 # ROCm: Notice we do not need the version for ROCM because we target a specific distribution in rocm.repo
 
 # Set up NVIDIA CUDA stack
-ln -s cuda-12.8 /usr/local/cuda
-echo /usr/local/nvidia/lib >> /etc/ld.so.conf.d/nvidia.conf
-echo /usr/local/nvidia/lib64 >> /etc/ld.so.conf.d/nvidia.conf
-export PATH=/usr/local/nvidia/bin:/usr/local/cuda/bin:${PATH}
-export LD_LIBRARY_PATH=/usr/local/nvidia/lib:/usr/local/nvidia/lib64
+export PATH=/usr/local/cuda/bin:${PATH}
 LIBRARY_PATH=/usr/local/cuda/lib64/stubs ldconfig
 
 wipednf
