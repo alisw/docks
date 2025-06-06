c473cfe4d852062093da4e7d22fe8e840893b309
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2025-06-06 10:16:33 +0200

    Initial best effort support for Arch Linux (#288)

---
+++ archlinux20250601-builder/provision.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+set -exo pipefail
+
+pacman -Syu --noconfirm
+pacman -S --noconfirm git python3 python-pip which gcc \
+                      make gcc-fortran \
+                      base-devel libxpm libxft glu gsl \
+                      libuv tbb xerces-c re2 libwebsockets grpc
