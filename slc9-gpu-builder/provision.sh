28f7651b8569fa3ee74be6b68116d7221d3a56c4
David Rohr <drohr@jwdt.org>
2026-02-10 13:06:34 +0100

    Bump GPU Builder container to CUDA 13

--- slc9-gpu-builder/provision.sh
+++ slc9-gpu-builder/provision.sh
@@ -9,21 +9,22 @@ wipednf () {
 # Install NVIDIA GPG key
 curl -fsSL https://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/D42D0685.pub |
   sed '/^Version/d' > /etc/pki/rpm-gpg/RPM-GPG-KEY-NVIDIA
-echo "${NVIDIA_GPGKEY_SUM}  /etc/pki/rpm-gpg/RPM-GPG-KEY-NVIDIA" | sha256sum -c --strict -
+echo "${NVIDIA_GPGKEY_SUM} /etc/pki/rpm-gpg/RPM-GPG-KEY-NVIDIA" | sha256sum -c --strict -
 
 # rpm --import https://repo.almalinux.org/almalinux/RPM-GPG-KEY-AlmaLinux
 dnf update -y
 # Install requirements for GPU event display, NVIDIA CUDA and AMD ROCm stacks
-CUV=${CUDA_PKG_VERSION}-${CUDA_PKG_VERSION/-/.}.*
+CUV=${CUDA_PKG_VERSION}-${CUDA_PKG_VERSION/-/.}.${CUDA_PKG_MINOR_VERSION}
+CUV2=${CUDA_PKG_VERSION}-${CUDA_PKG_VERSION/-/.}.*
 dnf install -y freeglut-devel lsof cmake libdrm-devel                                             \
                vulkan-loader-devel glslc wayland-devel wayland-protocols-devel libxkbcommon-devel \
-               "cuda-cudart-$CUV" 'cuda-compat-12-0-*' "cuda-libraries-$CUV" "cuda-nvtx-$CUV"     \
-               "cuda-libraries-devel-$CUV" "cuda-nvml-devel-$CUV" "cuda-minimal-build-$CUV"       \
+               "cuda-cudart-$CUV2" "cuda-libraries-$CUV" "cuda-nvtx-$CUV2"                        \
+               "cuda-libraries-devel-$CUV" "cuda-nvml-devel-$CUV2" "cuda-minimal-build-$CUV"      \
                "cuda-command-line-tools-$CUV" tensorrt-devel tensorrt-libs                        \
                "cudnn9-cuda-$CUDA_PKG_VERSION"                                                    \
                hip-rocclr ocl-icd ocl-icd-devel hipcub rocthrust rocm-dev hipify-clang            \
                hiprand-devel hipblas-devel hipsparse-devel rocblas-devel rocrand-devel            \
-               miopen-hip-devel hipfft-devel rccl-devel migraphx-devel rdma-core-devel            \
+               miopen-hip-devel hipfft-devel rccl-devel migraphx-devel                            \
                hipblaslt-devel rocprim-devel
 # ROCm: Notice we do not need the version for ROCM because we target a specific distribution in rocm.repo
 
