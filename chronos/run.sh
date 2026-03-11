d8c866066085b683549463dc02668c47ceafae4c
Sergio Garcia <47090312+singiamtel@users.noreply.github.com>
2026-03-11 15:40:17 +0100

    Cleanup old images (#306)

--- chronos/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh -ex
-export MESOS_NATIVE_JAVA_LIBRARY=/usr/local/lib/libmesos.so
-java -cp  /usr/bin/chronos org.apache.mesos.chronos.scheduler.Main --master zk://leader.mesos:2181/mesos  --zk_hosts leader.mesos:2181
