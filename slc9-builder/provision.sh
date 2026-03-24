f489e68422e429b4ddcb699a934ae63165f7e9c1
Sergio Garcia <sergio.garcia@cern.ch>
2026-03-24 14:17:02 +0100

    Add recc to build containers

--- slc9-builder/provision.sh
+++ slc9-builder/provision.sh
@@ -27,4 +27,7 @@ dnf groups install -y 'Development Tools'
 # python3-{pip,setuptools} and s3cmd needed for Jenkins builds.
 dnf install -y alice-o2-full-deps python3-pip python3-setuptools s3cmd time
 
+curl https://s3.cern.ch/swift/v1/alibuild-repo/bin/recc -L -o /usr/bin/recc
+chmod +x /usr/bin/recc
+
 wipednf
